86cfc1d50c8b2e4ce9d45514331cbae809460ebb
Laurence <hxl173830@utdallas.edu>
2018-11-13 14:58:57 -0600

    Modify database data

--- src/dao/StaffDaoImpl.java
+++ src/dao/StaffDaoImpl.java
@@ -14,22 +14,22 @@ public class StaffDaoImpl implements StaffDao {
 	static PreparedStatement ps;
 	DbManager db = new DbManager();
 	
-	@Override
-	public int register(String password) {
-		int status = 0;
-		try{
-			conn = db.getConnection();
-			ps =conn.prepareStatement("insert into customer values(?)");
-//			ps.setString(1, c.getUsername());
-			ps.setString(1, password);
-//			ps.setString(3, c.getName());
-			status = ps.executeUpdate();
-			conn.close();
-		}catch(Exception e){
-			System.out.println(e);
-		}
-		return status;
-	}
+//	@Override
+//	public int register(String password) {
+//		int status = 0;
+//		try{
+//			conn = db.getConnection();
+//			ps =conn.prepareStatement("insert into customer values(?)");
+////			ps.setString(1, c.getUsername());
+//			ps.setString(1, password);
+////			ps.setString(3, c.getName());
+//			status = ps.executeUpdate();
+//			conn.close();
+//		}catch(Exception e){
+//			System.out.println(e);
+//		}
+//		return status;
+//	}
 
 	@Override
 	public String validateCustomer(String password) {
@@ -37,14 +37,11 @@ public class StaffDaoImpl implements StaffDao {
 		try{
 			conn = db.getConnection();
 			ps =conn.prepareStatement("select * from user where password=?");
-//			ps.setString(1, login.getUsername());
 			ps.setString(1, password);
 
 			ResultSet rs = ps.executeQuery();
 			while(rs.next()){
-//				c.setUsername(rs.getString(1));
 				fetchedPassword = rs.getString(1);
-//				c.setName(rs.getString(3));
 			}
 			conn.close();
 		}catch(Exception e){
